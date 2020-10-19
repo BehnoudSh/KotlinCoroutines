db03c000679ebdfdeceeba711b9ff123200d5597
b.sherafati <behnoud.sherafati@gmail.com>
2020-10-19 12:05:51 +0330

    basic simulation of a network request

--- app/src/main/java/ir/navaco/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/ir/navaco/kotlincoroutines/MainActivity.kt
@@ -2,10 +2,38 @@ package ir.navaco.kotlincoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
+
+    private val RESULT_1 = "Result #1"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+
+
+        }
+
+
     }
+
+    private suspend fun getResult1FromApi(): String {
+
+        logThread("getResult1FromApi")
+
+        delay(1000)
+
+        return RESULT_1
+
+    }
+
+    private fun logThread(methodName: String) {
+
+        println("debug : ${methodName} : ${Thread.currentThread().name}")
+
+    }
+
 }
